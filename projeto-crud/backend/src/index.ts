d1c285f21a53f47196c32fc87a0165066aace874
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2024-08-27 09:59:12 -0300

    initial commit

---
+++ projeto-crud/backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors"
+import rotasProdutos from "./routes/produtos"
+import rotasLogin from "./routes/login"
+import dotenv from "dotenv"
+import autenticacao from "./middlewares/autenticacao"
+
+dotenv.config()
+
+
+const app = express();
+const porta = 4000;
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use("/login", rotasLogin)
+app.use("/produtos", autenticacao, rotasProdutos)
+
+
+app.listen(porta, () => {
+    console.log(`rodando na porta ${porta}`);
+});
