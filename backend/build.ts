06b8ff7e074dc990744968a5dda8caf2396cefe8
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2024-08-27 13:32:22 -0300

    ajuste

--- backend/build.ts
+++ backend/build.ts
@@ -14,8 +14,8 @@ import childProcess from 'child_process';
   try {
     // Remove current build
     await remove('./dist/');
-    // Copy front-end files
-    await copy('./src/public', './dist/public');
+    // Commented out because src/public doesn't exist
+    // await copy('./src/public', './dist/public');
     await copy('./src/views', './dist/views');
     // Copy back-end files
     await exec('tsc --build tsconfig.prod.json', './');
